e5638e28b0f9223cf72d4ed174f8bfceb002070b
Isadora Zandonotto <isadora.zandonotto@dbccompany.com.br>
2020-11-08 18:16:21 -0300

    chore[SearchBox]: implement label

--- src/components/searchBox/SearchBox.js
+++ src/components/searchBox/SearchBox.js
@@ -8,6 +8,7 @@ function SearchBox(props) {
   return(
     <div className="search-box">
       <div>
+        <label>Search for books using key words:</label>
         <input className="input-search" onChange={e => {searchFieldValue = e.target.value}} type="text"></input>
         <button className="input-button" onClick={() => props.keyWordHandler(searchFieldValue)}>Search</button>
       </div>
