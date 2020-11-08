cbcb171331d2bf51945613b700eac52384175131
Isadora Zandonotto <isadora.zandonotto@dbccompany.com.br>
2020-11-08 16:49:09 -0300

    style[SearchBox]: implement style

--- src/components/searchBox/SearchBox.js
+++ src/components/searchBox/SearchBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './SearchBox.css'
 
 function SearchBox(props) {
 
   let searchFieldValue = '';
 
   return(
-    <div>
-      <input onChange={e => {searchFieldValue = e.target.value}} type="text"></input>
-      <button onClick={() => props.keyWordHandler(searchFieldValue)}> Search</button>
+    <div className="search-box">
+      <div>
+        <input className="input-search" onChange={e => {searchFieldValue = e.target.value}} type="text"></input>
+        <button className="input-button" onClick={() => props.keyWordHandler(searchFieldValue)}> Search</button>
+      </div>
     </div>
   );
 }
